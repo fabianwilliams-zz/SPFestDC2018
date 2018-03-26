544dd21fa92855a196e27f85d15d856ec2267e65
Fabian Williams <fabian@adotob.com>
2018-03-26 13:24:12 -0400

    Adding Item for Workshops

---
+++ minileadgen/minileadgen.core/Helpers/LeadManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using minileadgen.core.Models;
+
+//  ALWAY ALWAY REMEMBER TO PUT THE KEY BACK. AND REMOVE BEFORE COMITTING TO PUBLIC GITHUB
+
+namespace minileadgen.core.Helpers
+{
+    public partial class LeadManager
+    {
+         static LeadManager defaultInstance = new LeadManager();
+
+        //const string accountURL = @"https://fabian.documents.azure.com:443/";
+        //const string accountKey = @"bvDULSic0MDonThDyBLE24ZlK5ATM5MXmFiZ20IwVsriaYUtlBK15GD2KfYC0ty0I6xJtPCd7F1DJd8hKfBduA=="; //  ALWAY ALWAY REMEMBER TO PUT THE KEY BACK. AND REMOVE BEFORE COMITTING TO PUBLIC GITHUB
+        const string accountURL = @"https://fabsforswift1.documents.azure.com:443/";
+        const string accountKey = @"iOGEnzlSQ1IoNvtHrQPS3DFF92GBvqlXYAlPBYujkGfzKIO49XtC6GsMFoCSp9w35DZ4yc2ccoYRBOjIqsnWxg=="; //  ALWAY ALWAY REMEMBER TO PUT THE KEY BACK. AND REMOVE BEFORE COMITTING TO PUBLIC GITHU
+        const string databaseId = @"fabsalpha";
+        const string collectionId = @"Leads";
+
+        private Uri collectionLink = UriFactory.CreateDocumentCollectionUri(databaseId, collectionId);
+
+        private DocumentClient client;
+
+        private LeadManager()
+        {
+            client = new DocumentClient(new System.Uri(accountURL), accountKey);
+        }
+
+        public static LeadManager DefaultManager {
+            get {
+                return defaultInstance;
+            }
+            private set {
+                defaultInstance = value;
+            }
+        }
+
+        public List<Lead> Leads { get; private set; }
+
+        public async Task<List<Lead>> GetLeadsAsync()
+        {
+            try
+            {
+                // The query excludes Leads that have been Converted to Opportunities
+                var query = client.CreateDocumentQuery<Lead>(collectionLink, new FeedOptions { MaxItemCount = -1 })
+                      .Where(leadItem => leadItem.IsConverted == false)
+                      .AsDocumentQuery();
+
+                Leads = new List<Lead>();
+                while (query.HasMoreResults)
+                {
+                    Leads.AddRange(await query.ExecuteNextAsync<Lead>());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"ERROR {0}", e.Message);
+                return null;
+            }
+            return Leads;
+        }
+
+        public async Task<Lead> InsertLeadAsync (Lead lead)
+        {
+            try
+            {
+                var result = await client.CreateDocumentAsync(collectionLink, lead);
+                lead.pId = result.Resource.Id;
+                Leads.Add(lead);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"ERROR {0}", e.Message);
+            }
+            return lead;
+        }
+    }
+}
---
+++ minileadgen/minileadgen.core/minileadgen.corePage.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using minileadgen.core.Helpers;
+using minileadgen.core.Models;
+
+namespace minileadgen.core
+{
+    public partial class minileadgen_corePage : ContentPage
+    {
+         LeadManager manager;
+
+        public minileadgen_corePage()
+        {
+            InitializeComponent();
+
+            manager = LeadManager.DefaultManager;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Set syncItems to true in order to synchronize the data on startup when running in offline mode
+            await RefreshItems(true);
+        }
+
+        // Event handlers
+        public async void OnSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+
+        public async void OnComplete(object sender, EventArgs e)
+        {
+
+        }
+
+        // Data methods
+        async Task AddItem(Lead item)
+        {
+            await manager.InsertLeadAsync(item);
+            leadsList.ItemsSource = await manager.GetLeadsAsync();
+        }
+
+        public async void OnAdd(object sender, EventArgs e)
+        {
+            Name nl = new Name
+            {
+                firstname = newFirstName.Text,
+                last = newLastName.Text
+
+            };
+
+            Address naddr = new Address
+            {
+                type = newAddrType.Text,
+                streetNumber = newStreetNum.Text,
+                streetName = newAddressName.Text,
+                aptNum = newApt.Text,
+                city = newCity.Text
+                                           
+            };
+
+            Contact ncon = new Contact
+            {
+                phone = newContactPhone.Text,
+                email = newContactEmail.Text
+            };
+
+            var l = new Lead
+            {
+                Country = newCountryName.Text,
+                Source = newSourceName.Text,
+                Name = nl,
+                Address = naddr,
+                Contact = ncon,
+                Interest = newInterest.Text,
+                IsConverted = false,
+                DateAdded = DateTime.Now
+
+            };
+            await AddItem(l);
+
+            newCountryName.Text = string.Empty;
+            newCountryName.Unfocus();
+            newSourceName.Text = string.Empty;
+            newSourceName.Unfocus();
+            newFirstName.Text = string.Empty;
+            newFirstName.Unfocus();
+            newLastName.Text = string.Empty;
+            newLastName.Unfocus();
+            newStreetNum.Text = string.Empty;
+            newStreetNum.Unfocus();
+            newAddressName.Text = string.Empty;
+            newAddressName.Unfocus();
+            newAddrType.Text = string.Empty;
+            newAddrType.Unfocus();
+            newApt.Text = string.Empty;
+            newApt.Unfocus();
+            newCity.Text = string.Empty;
+            newCity.Unfocus();
+            newContactPhone.Text = string.Empty;
+            newContactPhone.Unfocus();
+            newContactEmail.Text = string.Empty;
+            newContactEmail.Unfocus();
+            newInterest.Text = string.Empty;
+            newInterest.Unfocus();
+        }
+
+        public async void OnRefresh(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
+            Exception error = null;
+            try
+            {
+                await RefreshItems(false);
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+            finally
+            {
+                list.EndRefresh();
+            }
+
+            if (error != null)
+            {
+                await DisplayAlert("Refresh Error", "Couldn't refresh data (" + error.Message + ")", "OK");
+            }
+        }
+
+        public async void OnSyncItems(object sender, EventArgs e)
+        {
+            await RefreshItems(true);
+        }
+
+        private async Task RefreshItems(bool showActivityIndicator)
+        {
+            using (var scope = new ActivityIndicatorScope(syncIndicator, showActivityIndicator))
+            {
+                leadsList.ItemsSource = await manager.GetLeadsAsync();
+            }
+        }
+
+        private class ActivityIndicatorScope : IDisposable
+        {
+            private bool showIndicator;
+            private ActivityIndicator indicator;
+            private Task indicatorDelay;
+
+            public ActivityIndicatorScope(ActivityIndicator indicator, bool showIndicator)
+            {
+                this.indicator = indicator;
+                this.showIndicator = showIndicator;
+
+                if (showIndicator)
+                {
+                    indicatorDelay = Task.Delay(2000);
+                    SetIndicatorActivity(true);
+                }
+                else
+                {
+                    indicatorDelay = Task.FromResult(0);
+                }
+            }
+
+            private void SetIndicatorActivity(bool isActive)
+            {
+                this.indicator.IsVisible = isActive;
+                this.indicator.IsRunning = isActive;
+            }
+
+            public void Dispose()
+            {
+                if (showIndicator)
+                {
+                    indicatorDelay.ContinueWith(t => SetIndicatorActivity(false), TaskScheduler.FromCurrentSynchronizationContext());
+                }
+            }
+        }
+    }
+}
